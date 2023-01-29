d09906f21ad934d92bfe090afb7580604a8b74b0
ntua-el19211 <el19211@mail.ntua.gr>
2023-01-29 14:04:32 +0200

    all tables

--- api-backend/models/questionnaire.js
+++ api-backend/models/questionnaire.js
@@ -1,17 +1,40 @@
 const { Sequelize, DataTypes } = require("sequelize");
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define("questionnaire", {
+        Questionnaire_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primarykey: true
+        },
+        Author_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Title: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+};
 
-const Questionnaire = sequelize.define("questionnaire", {
-    Questionnaire_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primarykey: true
-    },
-    Author_id: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    Title: {
-        type: DataTypes.STRING,
-        allowNull: true
+const sequelize = new Sequelize(
+    'intelliq_api',
+    'root',
+    'MariaKoilalou2210!',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
     }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+sequelize.sync().then(() => {
+    console.log('Book table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
 });
