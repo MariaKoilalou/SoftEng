eff7bfc8af8302ca35d6a570594ad3b52dcf2af1
ntua-el19173 <el19173@mail.ntua.gr>
2023-02-11 15:55:08 +0200

    questionnaire controller and router 1st attempt

--- api-backend/routes/questionnaire.js
+++ api-backend/routes/questionnaire.js
@@ -1,13 +1,14 @@
-const express = require('express');
-
 const questionnaireController = require('../controllers/questionnaire');
 
 const upload = require("../middlewares/upload");
 const isAuth = require('../middlewares/authentication')
 const permit = require('../middlewares/authorization')
 
+const express = require('express');
+const controller = require('./controller');
+
 const router = express.Router();
 
-// router.get('/:questionnaireID', questionnaireController.getQuestionnaire);
+router.get('/:questionnaireId', controller.getQuestionnaire);
 
 module.exports = router;
