ee12f8d5d7772bdafcc808bda16f0489c6ed5c46
ntua-el19130 <el19130@mail.ntua.gr>
2023-02-11 22:56:52 +0200

    plz

--- api-backend/controllers/question.js
+++ api-backend/controllers/question.js
@@ -8,8 +8,30 @@ const jwt = require('jsonwebtoken');
 const sequelize = require('../util/database');
 var initModels = require("../models/init-models");
 var models = initModels(sequelize);
-// end of require models
 
 const fs = require("fs");
 const csv = require("fast-csv");
 
+exports.getQuestion = async (req, res) => {
+    try {
+      const questionnaireId = req.params.questionnaireID;
+      const questionId = req.params.questionID;
+  
+      const question = await models.question.findOne({
+        where: { 
+          QuestionnaireQuestionnaire_id: questionnaireId,
+          Question_id: questionId
+        },
+        attributes: ["Question_id", "Text", "type", "Mandatory"]
+      });
+  
+      if (!question) {
+        return res.status(404).json({ msg: "Question not found" });
+      }
+  
+      return res.json(question);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: "Server error" });
+    }
+  };
