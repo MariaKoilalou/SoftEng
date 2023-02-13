84fbddc96dd0b2e622b52cad45194a5989d1b4cf
ntua-el19130 <el19130@mail.ntua.gr>
2023-02-13 22:31:02 +0200

    getquestionanswers controller 1st try

--- api-backend/controllers/getquestionanswers.js
+++ api-backend/controllers/getquestionanswers.js
@@ -1,23 +1,36 @@
-// const express = require("express");
-// const sequelize = require('../util/database');
-// var initModels = require("../models/init-models");
-// var models = initModels(sequelize);
-// const Sequelize = require('sequelize');
-//
-// exports.getAnswer = (req, res, next) => {
-//
-//     const questionnaireID = req.param.questionnaireID;
-//     const sessionID = req.param.session;
-//
-//
-//     if (!questionnaireID || ! !sessionID) return res.status(400).json({message: 'Some parameters are undefined'});
-//
-//
-//     models.session.findOne({ where: {QuestionnaireQuestionnaire_id: questionnaireID} })
-//             .then(models.answer.findOne({ where: {SessionSession_id: sessionID} }))
-//             .then()
-//             .catch(err => {
-//                 return res.status(500).json({message: 'Internal server error.'})
-//             })
-//     }
-// }
+const sequelize = require('../util/database');
+const initModels = require("../models/init-models");
+const models = initModels(sequelize);
+
+exports.getQuestionAnswers = async (req, res) => {
+  try {
+    const questionnaireID = req.params.questionnaireID;
+    const questionID = req.params.questionID;
+
+    if (!questionnaireID || !questionID) {
+      return res.status(400).json({ msg: "Data Undefined" });
+    }
+
+    const question = await models.question.findOne({
+      where: { 
+        QuestionnaireQuestionnaire_id: questionnaireID,
+        Question_id: questionID
+      }
+    });
+
+    if (!question) {
+      return res.status(404).json({ msg: "Question not found" });
+    }
+
+    const answers = await models.answer.findAll({
+      where: {
+        QuestionQuestion_id: questionID
+      }
+    });
+
+    return res.json(answers);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: "Server error" });
+  }
+};
