68b9f69aecd376be06fb9239916330788f5337b1
ntua-el19130 <el19130@mail.ntua.gr>
2023-02-14 20:20:23 +0200

    minnor fix in error status

--- api-backend/app.js
+++ api-backend/app.js
@@ -34,6 +34,6 @@ app.use('/intelliq_api/qetsessionanswers', getsessionanswers);
 // /*End of routes used by our project */
 
 // In case of an endpoint does not exist
-app.use((req, res, next) => { res.status(404).json({message: 'Endpoint not found!'}); })
+app.use((req, res, next) => { res.status(400).json({message: 'Endpoint not found!'}); })
 
 module.exports = app;
