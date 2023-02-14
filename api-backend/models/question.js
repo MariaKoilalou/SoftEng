decea8df66e6b017545dd6b8d2481157e1345a91
ntua-el19211 <=>
2023-02-14 19:03:28 +0200

    boolean

--- api-backend/models/question.js
+++ api-backend/models/question.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
         },
         Mandatory: {
-            type: DataTypes.BOOL,
+            type: DataTypes.BOOLEAN,
             allowNull: false
         },
         QuestionnaireQuestionnaire_id: {
