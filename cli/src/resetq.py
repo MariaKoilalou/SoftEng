d6d59e7d8a398a22bf5c537a45cdf0d7b26433c1
ntua-el19173 <el19173@mail.ntua.gr>
2023-02-18 14:24:51 +0200

    resetq.py and resetall.py fixes - still not working

--- cli/src/resetq.py
+++ cli/src/resetq.py
@@ -3,15 +3,15 @@ import requests
 import json
 
 def resetq(ar):
-    url = 'http://localhost:9103/intelliq_api/resetall/' + ar.questionnaire_id
-    res = requests.post(url, verify=False)
+    url = f'http://localhost:9103/intelliq_api/resetall/{ar.questionnaire_id}'
+    res = requests.delete(url, verify=False)
     print(res.status_code)
     print(res.json())
     return True
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--questionnaire_id', help='Give Questionnaire ID', required='TRUE')
+parser.add_argument('--questionnaire_id', help='Give Questionnaire ID', required=True)
 args = parser.parse_args()
 
 resetq(args)
