5a200e730d66e409d0b1dcca5f4fb3ef0da1dae0
ntua-el19173 <el19173@mail.ntua.gr>
2023-02-18 11:46:16 +0200

    getsessionanswers.py cli fix and cli unit test

--- api-backend/controllers/getsessionanswers.js
+++ api-backend/controllers/getsessionanswers.js
@@ -52,9 +52,9 @@ exports.getSessionAnswers = async (req, res) => {
       };
 
       const csv = json2csv(result, { fields })
-      const html = `<pre>${csv}</pre>`;
-      res.setHeader("Content-Type", "text/html");
-      return res.send(html);
+      // const html = `<pre>${csv}</pre>`;
+      res.setHeader("Content-Type", "text/plain");
+      return res.send(csv);
     } 
 
     else {
